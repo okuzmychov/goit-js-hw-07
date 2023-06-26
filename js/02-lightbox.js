5c729ec41faaa05bbe8232a9ccec9bee8c0c781f
okuzmychov <o.kuzmichev@gmail.com>
2023-06-26 15:44:26 +0300

    task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+const createGalleryMarkup = (items) => {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+          <img class="gallery__image"
+          src="${preview}" 
+          alt="${description}" />
+          </a>
+        </li>
+      `;
+    })
+    .join('');
+};
+
+galleryList.innerHTML = createGalleryMarkup(galleryItems);
+
+const gallery = new SimpleLightbox('.gallery a');
+
+gallery.on('show.simplelightbox', function () {
+    setTimeout(() => {
+        const currentImage = this.currentImage;
+        const description = currentImage.querySelector('img').getAttribute('alt');
+        setTimeout(() => {
+            currentImage.$slide.append(`<div class="sl-description">${description}</div>`);
+        }, 250);
+    });
+});
+
+// console.log(galleryItems);
