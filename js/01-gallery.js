483cb1f8fd0fb4768b25e06854210c0213f56e1b
okuzmychov <o.kuzmichev@gmail.com>
2023-06-26 14:29:13 +0300

    task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,68 +1,45 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const list = document.querySelector(".js-gallery");
+const galleryList = document.querySelector('.gallery');
 
-// list.insertAdjacentHTML('beforeend', createMarkupItems(cars));
-// [...document.querySelectorAll('.js-add')].forEach(item => item.addEventListener('click', handlerClickCarBTN))
-// list.addEventListener('click', handlerClickCar);
+const createGalleryMarkup = (items) => {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </li>
+      `;
+    })
+    .join('');
+};
 
-list.insertAdjacentHTML("beforeend", createMarkupItems(arr));
-// list.addEventListener("click", handlerClickCarBTN);
-list.addEventListener("click", handlerClickImg);
+galleryList.innerHTML = createGalleryMarkup(galleryItems);
 
-// function handlerClickCarBTN(evt) {
-//   const cardItem = evt.target.closest(".js-item");
-//   if (evt.target.classList.contains("js-add")) {
-//     const obj = findCarItem(cardItem);
-//     const instance = basicLightbox.create(createAddToCartMarkup(obj));
-//     evt.stopImmediatePropagation();
-//     // evt.stopPropagation()
-//     instance.show();
-//   }
-// }
-
-function handlerClickImg(evt) {
-  const imgItem = evt.target.closest(".js-item");
-  if (imgItem) {
-    //evt.target !== evt.currentTarget
-    const obj = findImgItem(imgItem);
-    const instance = basicLightbox.create(createDetailInfoMarkup(obj));
-    instance.show();
+const handleImageClick = (event) => {
+  event.preventDefault();
+  
+  if (event.target.nodeName !== 'IMG') {
+    return;
   }
-}
+  
+  const originalImage = event.target.dataset.source;
+  const description = event.target.alt;
+  
+  const instance = basicLightbox.create(`
+    <img src="${originalImage}" alt="${description}">
+  `);
+  
+  instance.show();
+};
 
-function findImgItem(item) {
-  const { id } = item.dataset;
-  const currentImg = photos.find(({ id: imgId }) => imgId === Number(id));
-  return currentImg;
-}
+galleryList.addEventListener('click', handleImageClick);
 
-function createAddToImgMarkup({ img } = {}) {
-  return `<div class="modal">
-    <img src="${img}" alt="${photo}" class="item-img"/>
-  </div>`;
-}
-
-function createDetailInfoMarkup({
-  id,
-  photo,
-  img = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png",
-} = {}) {
-  return `<div data-id="${id}" class="modal">
-    <img src="${img}" alt="${photo}" class="item-img"/>
-  </div>`;
-}
-
-function createMarkupItems(arr) {
-  return arr
-    .map(
-      ({ id, img, photo }) => `
-<li data-id="${id}" class="item js-item">
-    <img src="${img}" alt="${photo}" class="item-img"/>
-</li>`
-    )
-    .join("");
-}
-
-console.log(galleryItems);
